424c0f6d2664bdf782dc4c37ad213fee4b6d3e79
Ansara Hollinger <ansara.hooks@gmail.com>
2019-04-30 22:08:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 100) + 1
+
+let mod = 20/5
+
+let max = Math.max(1, 2, 3, 20)
